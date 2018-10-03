e928d77818c572e6a94cf13e22f52070b6b5835f
Jesse Nelson <ic3y808@gmail.com>
2018-10-03 12:44:52 -0600

    small corrections

--- controllers/controller.status.js
+++ controllers/controller.status.js
@@ -1,79 +1,88 @@
 var controllers = angular.module('controllers-status', []);
 $(".content").css("display", "none");
 $(".loader").css("display", "block");
-controllers.controller('statusController', ['$rootScope', '$scope', 'subsonicService', function($rootScope, $scope, subsonicService) {
-	console.log('status-controller')
-	
-	$scope.ping = function(){
-		if($rootScope.isLoggedIn){
-			var ping = subsonicService.ping();
-			if(ping){
-				ping.then(function(data) {
-					console.log('ping ' + data);
-					$scope.server = data;
-					$scope.$apply();
-				});
-			}	
-		}
-    };
-    
-    $scope.getUserInfo = function(){
-        if($rootScope.isLoggedIn){
-            $rootScope.subsonic.getUserInfo().then(function (userInfo) {
-                console.log('ping ' + userInfo);
-                $scope.userInfo = userInfo;
-                $scope.$apply();
-            });
+controllers.controller('statusController', ['$rootScope', '$scope', 'subsonicService', function ($rootScope, $scope, subsonicService) {
+  console.log('status-controller')
+
+  $scope.ping = function () {
+    if ($rootScope.isLoggedIn) {
+      var ping = subsonicService.ping();
+      if (ping) {
+        ping.then(function (data) {
+          console.log('ping ' + data);
+          $scope.server = data;
+          $scope.$apply();
+        });
+      }
+    }
+  };
+
+  $scope.getUserInfo = function () {
+    if ($rootScope.isLoggedIn) {
+      $rootScope.subsonic.getUserInfo().then(function (userInfo) {
+        console.log('ping ' + userInfo);
+        $scope.userInfo = userInfo;
+        $scope.$apply();
+      });
+    }
+  };
+
+  $scope.getMediaFolders = function () {
+    if ($rootScope.isLoggedIn) {
+      $rootScope.subsonic.getMusicFolders().then(function (data) {
+
+        $scope.folders = data;
+        $scope.$apply();
+      });
+    }
+  };
+
+  $scope.startScan = function () {
+    if ($rootScope.isLoggedIn) {
+      $rootScope.subsonic.startScan().then(function (data) {
+        $scope.scanStatus = data;
+        $scope.scanStatusTotalFiles = data.count;
+        $scope.$apply();
+        $scope.rescanInterval = setInterval(function () {
+          $scope.getScanStatus();
+        }, 500);
+      });
+    }
+  };
+
+  $scope.getScanStatus = function () {
+    if ($rootScope.isLoggedIn) {
+      $rootScope.subsonic.getScanStatus().then(function (data) {
+        $scope.scanStatus = data;
+        if ($scope.scanStatus.count === $scope.scanStatusTotalFiles) {
+          clearInterval($scope.rescanInterval);
         }
-    };
+        $scope.$apply();
+      });
+    }
+  };
 
-    $scope.getMediaFolders = function(){
-        if($rootScope.isLoggedIn){
-            $rootScope.subsonic.getMusicFolders().then(function (data) {
-            
-                $scope.folders = data;
-                $scope.$apply();
-            });
-        }
-    };
+  $rootScope.$on('loginStatusChange', function (event, data) {
+    $scope.ping();
+    $scope.getUserInfo();
+    $scope.getMediaFolders();
+  });
 
-    $scope.startScan = function(){
-        if($rootScope.isLoggedIn){
-            $rootScope.subsonic.startScan().then(function (data) {
-                $scope.scanStatus = data;
-                $scope.$apply();
-            });
-        }
-    };
-
-    $scope.getScanStatus = function(){
-        if($rootScope.isLoggedIn){
-            $rootScope.subsonic.getScanStatus().then(function (data) {
-                $scope.scanStatus = data;
-                $scope.$apply();
-            });
-        }
-    };
-
-	$rootScope.$on('loginStatusChange', function (event, data) {
-        $scope.ping();
-        $scope.getUserInfo();
-        $scope.getMediaFolders();
-	});
-
-	$scope.refreshIntereval = setInterval(function(){
-        $scope.ping();
-        $scope.getUserInfo();
-        $scope.getMediaFolders();
-        $scope.getScanStatus();
-	}, 5000);
+  $scope.refreshIntereval = setInterval(function () {
+    $scope.ping();
+    $scope.getUserInfo();
+    $scope.getMediaFolders();
+  }, 5000);
 
 
-	$scope.$on('$destroy', function () { clearInterval($scope.refreshIntereval); });
-	$scope.ping();
-	$scope.getUserInfo();
-	$scope.getMediaFolders();
-	if($rootScope.isMenuCollapsed) $('.content').toggleClass('content-wide');
-	$(".loader").css("display", "none");
-	$(".content").css("display", "block");
+  $scope.$on('$destroy', function () {
+    clearInterval($scope.refreshIntereval);
+    clearInterval($scope.rescanInterval);
+  });
+  $scope.ping();
+  $scope.getUserInfo();
+  $scope.getMediaFolders();
+  if ($rootScope.isMenuCollapsed) $('.content').toggleClass('content-wide');
+  $(".loader").css("display", "none");
+  $(".content").css("display", "block");
 }]);
