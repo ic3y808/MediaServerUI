08fa5a97f63755676e3b1409e4af3b4d0e2f7aa6
Jesse Nelson <{ID}+{username}@users.noreply.github.com>
2018-10-26 12:52:25 -0600

    update webpack for production, minified resources. fix includes.

--- frontend/app.js
+++ frontend/app.js
@@ -1,24 +1,29 @@
 import angular from 'angular';
 import angularRoute from 'angular-route';
 import angularAnimate from 'angular-animate';
-//__webpack_public_path__ = "http://localhost:" + SERVER_PORT + "/";
+__webpack_public_path__ = "http://localhost:" + SERVER_PORT + "/";
 
 import './styles/index';
 import 'underscore';
 import 'popper.js';
 import 'tooltip.js';
-import 'jquery-easing';
+//import 'jquery-easing';
 import 'jquery.flipster';
 import 'moment';
-import 'bootstrap';
+//import 'bootstrap';
 import 'typeface-roboto';
+import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/popover';
+import 'bootstrap/js/dist/tooltip';
+import 'bootstrap/js/dist/util';
 
 require("ag-grid-community");
 import * as agGridCommunity from 'ag-grid-community';
 import SubsonicAPI from './API/subsonic.api';
 import CastFramework from './API/cast.framework';
 import CastAPI from './API/cast.v1';
-import Clipboard from 'clipboard';
+//import Clipboard from 'clipboard';
 import Components from './components';
 import Services from './services';
 import Factories from './factories';
