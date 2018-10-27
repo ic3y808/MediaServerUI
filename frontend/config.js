4e1960adcea44bb3276ad263a3dfd55ed5ac000b
Jesse Nelson <{ID}+{username}@users.noreply.github.com>
2018-10-26 21:51:24 -0600

    fix webpack minifiy

--- frontend/config.js
+++ frontend/config.js
@@ -1,42 +1,38 @@
-class ApplicationConfig {
-  constructor($routeProvider, $locationProvider) {
-    "ngInject";
-    $routeProvider.when('/', {
-      template: '<home></home>',
-    }).when('/fresh', {
-      template: '<fresh></fresh>',
-    }).when('/status', {
-      template: '<status></status>',
-    }).when('/index', {
-      template: '<index></index>',
-    }).when('/starred', {
-      template: '<starred></starred>',
-    }).when('/playlist', {
-      template: '<playlist></playlist>',
-    }).when('/playlists', {
-      template: '<playlists></playlists>',
-    }).when('/genres', {
-      template: '<genres></genres>',
-    }).when('/genre/:id', {
-      template: '<genre></genre>',
-    }).when('/podcasts', {
-      template: '<podcasts></podcasts>',
-    }).when('/playing', {
-      template: '<playing></playing>',
-    }).when('/settings/subsonic', {
-      template: '<subsonicSettings></subsonicSettings>',
-    }).when('/artists', {
-      template: '<artists></artists>',
-    }).when('/artist/:id', {
-      template: '<artist></artist>',
-    }).otherwise({
-      redirectTo: '/'
-    });
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-    }).hashPrefix('');
-    }
-  }
-
-export default ApplicationConfig;
+export default function ApplicationConfig($routeProvider, $locationProvider) {
+  "ngInject";
+  $routeProvider.when('/', {
+    template: '<home></home>',
+  }).when('/fresh', {
+    template: '<fresh></fresh>',
+  }).when('/status', {
+    template: '<status></status>',
+  }).when('/index', {
+    template: '<index></index>',
+  }).when('/starred', {
+    template: '<starred></starred>',
+  }).when('/playlist', {
+    template: '<playlist></playlist>',
+  }).when('/playlists', {
+    template: '<playlists></playlists>',
+  }).when('/genres', {
+    template: '<genres></genres>',
+  }).when('/genre/:id', {
+    template: '<genre></genre>',
+  }).when('/podcasts', {
+    template: '<podcasts></podcasts>',
+  }).when('/playing', {
+    template: '<playing></playing>',
+  }).when('/settings/subsonic', {
+    template: '<subsonicSettings></subsonicSettings>',
+  }).when('/artists', {
+    template: '<artists></artists>',
+  }).when('/artist/:id', {
+    template: '<artist></artist>',
+  }).otherwise({
+    redirectTo: '/'
+  });
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  }).hashPrefix('');
+}
