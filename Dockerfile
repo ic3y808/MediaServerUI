7a9dc823249441cec132344f9ffcb23f8c8f5211
Jesse Nelson <ic3y808@noreply.github.com>
2019-01-31 17:14:16 -0700

    adding docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM node:8
 WORKDIR /home/node/app
 ADD package.json /home/node/app/package.json
-RUN npm install
 ADD webpack.config.js /home/node/app/webpack.config.js
+ADD database.json /home/node/app/database.json
+ADD migrations /home/node/app/migrations
 ADD backend /home/node/app/backend
 ADD frontend /home/node/app/frontend
+ADD api /home/node/app/api
+ADD receiver /home/node/app/receiver
+RUN npm install
 RUN npm run build
-EXPOSE 3000
-CMD [ "npm", "start" ]
+EXPOSE 3000 4000
+CMD [ "npm", "run", "prod" ]
--- api/api/v1/config.js
+++ api/api/v1/config.js
@@ -1,7 +1,7 @@
-'use strict';
-var express = require('express');
+"use strict";
+var express = require("express");
 var router = express.Router();
-var structures = require('./structures');
+var structures = require("./structures");
 
 /**
  * This function comment is parsed by doctrine
@@ -15,8 +15,8 @@ var structures = require('./structures');
  * @returns {MediaPath} Ping - A MediaPath object
  * @security ApiKeyAuth
  */
-router.get('/mediapaths', function (req, res) {
-  res.json(res.locals.db.prepare('SELECT * FROM MediaPaths').all());
+router.get("/mediapaths", function(req, res) {
+  res.json(res.locals.db.prepare("SELECT * FROM MediaPaths").all());
 });
 
 /**
@@ -33,18 +33,17 @@ router.get('/mediapaths', function (req, res) {
  * @returns {MediaPath} Ping - A MediaPath object
  * @security ApiKeyAuth
  */
-router.put('/mediapaths', function (req, res) {
+router.put("/mediapaths", function(req, res) {
   var displayName = req.query.displayName;
   var path = req.query.path;
-  try {
-    const stmt = res.locals.db.prepare('INSERT INTO MediaPaths (display_name, path) VALUES (?, ?) ON CONFLICT(display_name) DO UPDATE SET path=?');
-    const info = stmt.run(displayName, path, path);
-    if (info.changes) {
-      res.json(new structures.StatusResult('Added media path'));
-    } else res.json(new structures.StatusResult(info));
-  } catch{
-    res.json(new structures.StatusResult('Failed'));
-  }
+
+  const stmt = res.locals.db.prepare(
+    "INSERT INTO MediaPaths (display_name, path) VALUES (?, ?) ON CONFLICT(display_name) DO UPDATE SET path=?"
+  );
+  const info = stmt.run(displayName, path, path);
+  if (info.changes) {
+    res.json(new structures.StatusResult("Added media path"));
+  } else res.json(new structures.StatusResult(info));
 });
 
 /**
@@ -55,33 +54,34 @@ router.put('/mediapaths', function (req, res) {
  * @group config - Config API
  * @param {string} path.query
  * @param {string} displayName.query
- * @returns {Status} 200 - Returns the status of the delete 
+ * @returns {Status} 200 - Returns the status of the delete
  * @returns {Error}  default - Unexpected error
  * @security ApiKeyAuth
  */
-router.delete('/mediapaths', function (req, res) {
+router.delete("/mediapaths", function(req, res) {
   var displayName = req.query.displayName;
   var path = req.query.path;
   if (displayName) {
     try {
-      const stmt = res.locals.db.prepare('DELETE FROM MediaPaths WHERE display_name=?');
+      const stmt = res.locals.db.prepare(
+        "DELETE FROM MediaPaths WHERE display_name=?"
+      );
       const info = stmt.run(displayName);
       if (info.changes) {
-        res.json(new structures.StatusResult('Deleted media path'));
+        res.json(new structures.StatusResult("Deleted media path"));
       } else res.json(new structures.StatusResult(info));
-    } catch{
-      res.json(new structures.StatusResult('Failed'));
+    } catch {
+      res.json(new structures.StatusResult("Failed"));
     }
-  }
-  else if (path) {
+  } else if (path) {
     try {
-      const stmt = res.locals.db.prepare('DELETE FROM MediaPaths WHERE path=?');
+      const stmt = res.locals.db.prepare("DELETE FROM MediaPaths WHERE path=?");
       const info = stmt.run(path);
       if (info.changes) {
-        res.json(new structures.StatusResult('Deleted media path'));
+        res.json(new structures.StatusResult("Deleted media path"));
       } else res.json(new structures.StatusResult(info));
-    } catch{
-      res.json(new structures.StatusResult('Failed'));
+    } catch {
+      res.json(new structures.StatusResult("Failed"));
     }
   }
 });
