e143bdf6ff1888ff348fd005113acea81dbf69df
Jesse Nelson <{ID}+{username}@users.noreply.github.com`>
2019-06-11 07:56:28 -0600

    update migration

--- migrations/20190209162356-alloy-db-default-settings.js
+++ migrations/20190209162356-alloy-db-default-settings.js
@@ -5,9 +5,9 @@ exports.setup = function (options, seedLink) {
 exports.up = function (db) {
   return {
     sql: true,
-    command: "INSERT OR REPLACE INTO `Settings`(`settings_key`,`settings_value`) VALUES (?,?);",
+    command: "INSERT OR IGNORE INTO `Settings`(`settings_key`,`settings_value`) VALUES (?,?);",
     values: ["alloydb_settings", "{\"alloydb_host\":\"localhost\",\"alloydb_port\":4000,\"alloydb_apikey\":\"" + process.env.API_KEY + "\",\"alloydb_include_port_in_url\":true,\"alloydb_lastfm_username\":\"\",\"alloydb_lastfm_password\":\"\",\"alloydb_scrobble\":false,\"alloydb_love_tracks\":false}"]
-  }
+  };
 };
 
 exports.down = function (db) {
