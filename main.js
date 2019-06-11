048e41c3d5cdd8e529f9824b7d8014ec146dd485
Jesse Nelson <{ID}+{username}@users.noreply.github.com>
2019-06-11 12:21:12 -0600

    various fixes

--- main.js
+++ main.js
@@ -37,6 +37,7 @@ if (!gotTheLock) {
     // Someone tried to run a second instance, we should focus our window.
     if (mainWindow) {
       if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.show();
       mainWindow.focus()
     }
   })
